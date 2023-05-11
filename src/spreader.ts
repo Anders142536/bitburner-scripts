18f614edd819a82a7d0dc2e57adcf0fc6bb7cfbe
Anders142536 <anders142536@gmail.com>
2023-05-11 02:17:27 +0200

    spreader now cracks ports

--- src/spreader.ts
+++ src/spreader.ts
@@ -1,34 +1,63 @@
 import type { BitBurner as NS } from 'Bitburner'
 
+let ns: NS
+let scriptRam: number
+let scripts: Scripts = {}
+let availableScriptsCount: number
+
+
 /** @param {NS} ns */
-export async function main(ns: NS) {
-	setup(ns)
-	let companies = findAllCompanies(ns)
+export async function main(ns_: NS) {
+	ns = ns_
+	ns.disableLog("ALL")
+	ns.print("\n\n")
 
+	let companies = findAllCompanies()
+	scriptRam = ns.getScriptRam('moneymaker.js')
 
-	let hLevel = ns.getHackingLevel()
-	for (let company of companies) {
-		if (!isHackable(ns, company, hLevel)) {
+	let prevHLevel = 0
+	while (true) {
+		let hLevel = ns.getHackingLevel()
+		if (prevHLevel == hLevel) {
+			ns.print('hacking level didn\'t change, waiting...')
+			// wait one minute
+			await ns.sleep(1000 * 60)
 			continue
 		}
-		if (!ns.hasRootAccess(company)) {
-			ns.print(`nuking ${company}`)
-			ns.nuke(company)
+		ns.print(`hacking level changed from ${prevHLevel} to ${hLevel}`)
+		ns.print(`attempting to hack ${companies.length} companies`)
+		let hacked: string[] = []
+		checkForCrackScripts()
+
+		for (let company of companies) {
+			if (!isHackable(company, hLevel)) {
+				ns.print(`${company} not hackable!`)
+				continue
+			}
+			if (!ns.hasRootAccess(company)) {
+				ns.tprint(`nuking ${company}`)
+				ns.print(`nuking ${company}`)
+				crackPorts(company)
+				ns.nuke(company)
+			}
+			let hackSuccessful = await hack(company)
+			if (hackSuccessful) {
+				hacked.push(company)
+			}
 		}
-		await hack(ns, company)
 
+		for (let company of hacked) {
+			companies.splice(companies.indexOf(company), 1)
+		}
+
+		ns.print(`${companies.length} companies left in list`)
+		prevHLevel = hLevel
 	}
-}
-
-function setup(ns: NS) {
-	ns.disableLog("ALL")
-
-	ns.print("\n\n")
 }
 
 // returns array of companies in system
 // TODO: make this a recursive scan
-function findAllCompanies(ns: NS) {
+function findAllCompanies() {
 	ns.print("finding all companies...")
 	let c = ns.scan("home")
 
@@ -36,26 +65,74 @@ function findAllCompanies(ns: NS) {
 	return c
 }
 
-function isHackable(ns: NS, company: string, hLevel: number) {
+function isHackable(company: string, hLevel: number) {
 	let cHLevel = ns.getServerRequiredHackingLevel(company)
 	let reqOpenPorts = ns.getServerNumPortsRequired(company)
 
-	if (cHLevel <= hLevel && reqOpenPorts == 0) {
-		// ns.print(`hackable company: ${company}`)
-		return true
-	}
-	return false
+	return cHLevel <= hLevel && reqOpenPorts <= availableScriptsCount
 }
 
-async function hack(ns: NS, company: string) {
-	ns.print(`hacking: ${company}`)
+function checkForCrackScripts() {
+	scripts = {
+		ssh: checkFile('BruteSSH.exe'),
+		ftp: checkFile('FTPCrack.exe'),
+		smtp: checkFile('relaySMTP.exe'),
+		http: checkFile('HTTPWorm.exe'),
+		sql: checkFile('SQLInject.exe'),
+	}
 
+	availableScriptsCount = 0
+	for (let script in scripts) {
+		if (scripts[script])
+			availableScriptsCount++
+	}
+}
+
+function checkFile(file: string): boolean {
+	return ns.fileExists(file)
+}
+
+function crackPorts(company: string) {
+	if (scripts.ssh)
+		ns.brutessh(company)
+	if (scripts.ftp)
+		ns.ftpcrack(company)
+	if (scripts.smtp)
+		ns.relaysmtp(company)
+	if (scripts.http)
+		ns.httpworm(company)
+	if (scripts.sql)
+		ns.sqlinject(company)
+}
+
+async function hack(company: string): Promise<boolean> {
+	// the hack method should only trigger once per company, so this *should* not
+	// kill the started scripts
+	ns.killall(company)
+
+	ns.print(`hacking: ${company}`)
 	ns.scp('moneymaker.js', company)
-	let pid = ns.exec('moneymaker.js', company, 1, company)
+
+	let maxRam = ns.getServerMaxRam(company)
+	let usedRam = ns.getServerUsedRam(company)
+	let availRam = maxRam - usedRam
+	let threadCount = Math.floor(availRam / scriptRam)
+
+	let pid = ns.exec('moneymaker.js', company, threadCount, company)
 
 	if (pid > 0) {
-		ns.print(`successfully jiggled moneymaker on ${company}`)
-	} else {
-		ns.print(`failed to jiggle moneymaker on ${company}`)
+		ns.tprint(`> successfully jiggled moneymaker on ${company} with ${threadCount} threads`)
+		ns.print(`> successfully jiggled moneymaker on ${company} with ${threadCount} threads`)
+		return true
 	}
+	ns.tprint(`> failed to jiggle moneymaker on ${company} with ${threadCount} threads`)
+	ns.print(`> failed to jiggle moneymaker on ${company} with ${threadCount} threads`)
+	return false
+
+}
+
+
+
+interface Scripts {
+	[key: string]: boolean
 }
